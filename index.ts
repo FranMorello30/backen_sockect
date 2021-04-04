b42ba8d3f83ce8c143df6a3b151a250570bc6d4b
Franciso Morello <jose19francisco@gmail.com>
2021-04-04 18:37:51 -0400

    Primer  commit - rest server

---
+++ index.ts
@@ -0,0 +1,27 @@
+import { SERVER_PORT } from './global/enviornment';
+
+import express from 'express';
+import Server from './class/server';
+import router from './routers/router';
+import cors from 'cors';
+// import bodyParser from 'body-parser';
+
+
+const server = new Server();
+
+// BodyParser
+server.app.use(express.urlencoded({extended:true}));
+server.app.use(express.json());
+
+
+//cors
+server.app.use( cors({ origin:true,credentials:true }))
+
+// Rutas servicios
+server.app.use('/',router);
+
+
+server.start( ()=>{
+    console.log(`Servidor corriendo en el puerto ${ SERVER_PORT }`)
+})
+
